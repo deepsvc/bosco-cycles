daf5edf4f23e2e15b0bc10b2faa4a3e13b3e3d5b
Deepak Choudhary <deepak.dhd@gmail.com>
2021-04-10 02:24:23 +0530

    asdf

--- src/components/SpecificServices/SpecificServices.js
+++ src/components/SpecificServices/SpecificServices.js
@@ -39,7 +39,7 @@ class SpecificServices extends Component {
             <Link className="transition duration-500 ease-in-out mx-5 md:mx-0 transform md:hover:scale-110" to="/SuspensionService">
               <div className="flex flex-col items-center">
                 <div className="hidden md:flex gifHolder g3 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
-                <div className="gifHolder-phone md:hidden g2 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
+                <div className="gifHolder-phone md:hidden g3 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
                 <div className="2xl:text-2xl text-center text-lg lg:py-10 pb-7 font-bold">
                   SUSPENSION SERVICE
                   <span className="text-base text-red-600">
@@ -52,7 +52,7 @@ class SpecificServices extends Component {
             <Link className="transition duration-500 ease-in-out mx-5 md:mx-0 transform md:hover:scale-110" to="/DriveTrainService">
               <div className="flex flex-col items-center">
                 <div className="hidden md:flex gifHolder g1 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
-                <div className="gifHolder-phone md:hidden g2 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
+                <div className="gifHolder-phone md:hidden g1 shadow-xl md:mr-5 mb-5 lg:mr-0 lg:mb-0"></div>
                 <div className="2xl:text-2xl text-center text-lg lg:py-10 pb-7 font-bold">
                   DRIVE TRAIN SERVICE
                   <span className="text-base text-red-600">
